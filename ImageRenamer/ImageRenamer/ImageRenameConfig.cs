fe42ca73e9be9a1080157ba67332e95b4cf8dadf
Vadym <vadimart92@gmail.com>
2014-07-04 12:07:04 +0300

    new  name

--- ImageRenamer/ImageRenamer/ImageRenameConfig.cs
+++ ImageRenamer/ImageRenamer/ImageRenameConfig.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.IO;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using LevDan.Exif;
@@ -18,38 +20,56 @@ namespace ImageTools.ImageRenamer {
 				try {
 					var ef = new ExifTagCollection(file);
 					result.Add(new ImageRenameConfig(ef, file));
+				} catch (ArgumentException) {
+					result.Add(new ImageRenameConfig(null, file) {
+						ExifInfoReadSuccess = false
+					});
 				}
-				catch (ArgumentException) {
-					result.Add(new ImageRenameConfig(null, file) { ExifInfoReadSuccess = false});
-				}
-				
+
 			}
 			return result;
 		}
-		
+
 	}
-	class ImageRenameConfig : IComparable<ImageRenameConfig>
-	{
+	public class ImageRenameConfig : IComparable<ImageRenameConfig>,INotifyPropertyChanged {
 		public string OldDisplayValue {
 			get {
-				return _oldName;
+				return _fileNameOnly;
 			}
 		}
+
+		public string NewFileName {
+			get { return _newFilename; }
+			set {
+				if (!value.Equals(_newFilename)) {
+					_newFilename = value;
+					PropertyChanged(this, GetArgs());
+				}
+				
+			}
+		}
+
 		public bool ExifInfoReadSuccess {
-			get { return _exifInfoReadSuccess; }
-			set { _exifInfoReadSuccess = value; }
+			get {
+				return _exifInfoReadSuccess;
+			}
+			set {
+				_exifInfoReadSuccess = value;
+			}
 		}
 
 		private ExifTagCollection _fileInfo;
 		private string _oldName;
 		private bool _exifInfoReadSuccess = true;
-
+		private string _fileNameOnly;
+		private string _newFilename;
 		public ImageRenameConfig(ExifTagCollection fileInfo, string fileName) {
 			_fileInfo = fileInfo;
 			_oldName = fileName;
+			_fileNameOnly = new FileInfo(fileName).Name;
 		}
 		public override string ToString() {
-			return _oldName +  ((ExifInfoReadSuccess)? string.Empty: ": exif read error") ;
+			return _oldName;
 		}
 
 		#region Члены IComparable<ImageRenameConfig>
@@ -57,6 +77,15 @@ namespace ImageTools.ImageRenamer {
 		public int CompareTo(ImageRenameConfig other) {
 			Func<ImageRenameConfig, int> comparer = (config) => (config.ExifInfoReadSuccess ? 1 : 0);
 			return comparer(this).CompareTo(comparer(other));
+		}
+
+		#endregion
+
+		#region Члены INotifyPropertyChanged
+
+		public event PropertyChangedEventHandler PropertyChanged;
+		private PropertyChangedEventArgs GetArgs([CallerMemberName]string propertyName = null) {
+			return new PropertyChangedEventArgs(propertyName);
 		}
 
 		#endregion
--- ImageRenamer/ImageRenamer/ImageRenamer.xaml.cs
+++ ImageRenamer/ImageRenamer/ImageRenamer.xaml.cs
@@ -10,9 +10,13 @@ namespace ImageTools.ImageRenamer {
 	/// <summary>
 	/// Логика взаимодействия для ImageRenamer.xaml
 	/// </summary>
-	public partial class ImageRenamer 
+	public partial class ImageRenamer
 	{
 		public bool IncludeSubDirs {
+			get;
+			set;
+		}
+		public bool SaveToSameFolder {
 			get;
 			set;
 		}
@@ -38,7 +42,6 @@ namespace ImageTools.ImageRenamer {
 			_images = new ObservableCollection<ImageRenameConfig>();
 			lv_files.DataContext = this.DataContext;
 			lv_files.ItemsSource = _images;
-			
 		}
 
 		private async void selectFolder_Click(object sender, RoutedEventArgs e) {
@@ -64,6 +67,12 @@ namespace ImageTools.ImageRenamer {
 				config.ForEach(fc => _images.Add(fc));
 			}
 			
+		}
+
+		private void SelectDestFolder_Click(object sender, RoutedEventArgs e) {
+			foreach (ImageRenameConfig image in _images) {
+				image.NewFileName = DateTime.Now.ToLongTimeString();
+			}
 		}
 	}
 }
